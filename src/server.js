e6795c8ae473073c75bdeb974ecd0ec74933b982
Vladimir <privezenoff@yandex.ru>
2021-10-03 02:42:58 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -3,10 +3,9 @@ const express = require('express');
 const mainRouter = require('./routers');
 
 const app = express();
-app.use('/files', express.static('../src'));
 
 app.use('/', mainRouter);
-app.listen(8080, () => {
+app.listen(8080,'127.0.0.1', () => {
     console.log('Server started on port 8080');
 });
 
